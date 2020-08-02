1c03c7dfd6b61a172b58342b97219d3dd14988a5
Joona Piirainen <joona.piirainen@gmail.com>
2020-08-02 14:05:51 +0300

    cpdegen not working

--- src/qql-schema/user/user.services.ts
+++ src/qql-schema/user/user.services.ts
@@ -3,7 +3,10 @@ import { UserInterface } from './user.types'
 
 export const findUser = (_: any, { id }: { id: string }) => User.findById(id)
 
-export const findAllUsers = () => User.find()
+export const findAllUsers = async () => {
+  const doc = await User.find()
+  return doc
+}
 
 export const createUser = async (_: null, { userInfo }: { userInfo: UserInterface }) => {
   console.log(userInfo, parent)
