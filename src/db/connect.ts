1978fa44279976d11f1f76da0c14e4a1f618e887
Joona Piirainen <joona.piirainen@gmail.com>
2020-08-02 23:37:25 +0300

    test still not working

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -27,3 +27,40 @@ export const connect = () => {
     })
   })
 }
+
+export const testConnect = () => {
+  return new Promise((_resolve, _reject) => {
+    mongoose.connect('mongodb://localhost:27017/shard-test', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
+    let db = mongoose.connection
+    db.on('error', err => {
+      console.log(err)
+    })
+    db.once('connected', () => {
+      console.log('Mongo connected')
+    })
+    db.on('reconnected', () => {
+      console.log('Mongo re-connected')
+    })
+    db.on('disconnected', () => {
+      console.log('Mongo disconnected')
+    })
+  })
+}
+
+export const closeDatabase = async () => {
+  await mongoose.connection.dropDatabase(() => console.log('db dropped'))
+  await mongoose.connection.close()
+}
+
+export const removeAllCollections = async () => {
+  const collections = Object.keys(mongoose.connection.collections)
+  for (const collectionName of collections) {
+    const collection = mongoose.connection.collections[collectionName]
+    await collection.deleteMany(() => console.log('collections deleted'))
+  }
+}
--- src/qql-schema/user/user.test.ts
+++ src/qql-schema/user/user.test.ts
@@ -1,19 +1,25 @@
 import { createUser } from './user.services'
-import { UserInterface } from './user.types'
-import { connect } from '../../db/connect'
+import { testConnect, closeDatabase, removeAllCollections } from '../../db/connect'
 
-beforeAll(async () => {
-  await connect()
-})
-
-const userInfo: UserInterface = {
+const userInfo = {
   username: 'Joona',
   password: '123',
   email: 'joona.piirainen@gmail.com',
   role: 'coach',
 }
 
+beforeAll(async () => await testConnect())
+afterAll(async () => {
+  await closeDatabase()
+  await removeAllCollections()
+})
+
 test('Should create a new user', async () => {
-  const user = await createUser(parent, { userInfo })
-  expect(user).toStrictEqual(userInfo)
+  const user = await createUser(null, { userInfo })
+  expect(user.username).toContain('Joona')
+  return
+})
+
+afterEach(async () => {
+  await removeAllCollections()
 })
